4e2f61ef1003d33b516e1477c038950eff11bffe
James Fairbanks <jpfairbanks@gmail.com>
2015-12-04 16:04:11 -0500

    Add SBM tests with 100 coverage.
    Here are the rebased commit messages:
    Import sample to please the multiple dispatch gods.
    Remove unsafe_add_edge! from sbm construction.
    Add == for StochasticBlockModel type.
    Add sbm test to runtests.jl.
    test/sbm.jl no longer is a command line prog.
    reduce verbosity of SBM:
    - constructors are silent
    - tests are silent if the pass
    Fix comments and dependencies for SBM
    get test cov to :100

--- src/randgraphs_sbm.jl
+++ src/randgraphs_sbm.jl
@@ -1,4 +1,5 @@
 using LightGraphs
+import StatsBase: sample
 """StochasticBlockModel(n,nodemap,affinities)
 A type capturing the parameters of the SBM.
 Each vertex is assigned to a block and the probability of edge (i,j)
@@ -19,6 +20,15 @@ type StochasticBlockModel{T<:Integer,P<:Real}
     affinities::Matrix{P}
 end
 
+import Base: ==
+function ==(sbm::StochasticBlockModel, other::StochasticBlockModel)
+    if (sbm.n == other.n) && (sbm.nodemap == other.nodemap) && (sbm.affinities == other.affinities)
+       return true
+    else
+        return false
+    end
+end
+
 """ a constructor for StochasticBlockModel that uses the sizes of the blocks
 and the affinity matrix. This construction implies that consecutive
 vertices will be in the same blocks, except for the block boundaries.
@@ -36,13 +46,6 @@ function StochasticBlockModel{T,P}(sizes::Vector{T}, affinities::Matrix{P})
     return StochasticBlockModel(csum[end], nodemap, affinities)
 end
 
-"""produce the sbm affinity matrix where the internal and external probabilities are
-the same
-"""
-function sbmaffinity(internalp::Float64, externalp::Float64, size::Int, numblocks::Int)
-    B = eye(numblocks)*(internalp) + externalp*(ones(numblocks,numblocks)-I)
-    return B
-end
 
 """produce the sbm affinity matrix where the external probabilities are the same
 the internal probabilities and sizes differ by blocks.
@@ -87,7 +90,7 @@ end
 """Return a generator for edges from a stochastic block model near bipartite graph."""
 function nearbipartiteaffinity(sizes::Vector{Int}, between::Float64, inter::Float64, noise)
     B = nearbipartiteaffinity(sizes, between, inter) + noise
-    info("Affinities are:\n$B")#, file=stderr)
+    # info("Affinities are:\n$B")#, file=stderr)
     return B
 end
 
@@ -103,37 +106,16 @@ function random_pair(n::Int)
     end
 end
 
-"""take the first n elements produced by t"""
-function take(n::Int, t::Task) 
-    for i in 1:n
-        produce(consume(t))
-    end
-end
 
-""" Draw an edgestream
-Affinity::(Node, Node) -> Probability
-pairs::generator of (Node, Node)
-"""
-function sample(affinity, pairs)
-    for (i, j) in pairs
-        aff = affinity(i,j)
-        #print(STDERR, "affinity $i,$j = $aff\n")
-        if rand() < aff
-            produce((i,j))
-        #else try again
-        end        
-    end
-end
-
-"""Take an infinite sample from the sbm. Use take() to take only the first n edges
-or pass to graph(edgestream, numedges).
+"""Take an infinite sample from the sbm.
+Pass to graph(edgestream, numedges) to get a Graph object.
 """
 function sample(sbm::StochasticBlockModel)
     pairs = @task random_pair(sbm.n)
     for (i,j) in pairs
     	if i == j
-	    continue
-	end
+            continue
+        end
         p = sbm.affinities[sbm.nodemap[i], sbm.nodemap[j]]
         if rand() < p
             produce(i, j)
@@ -144,31 +126,31 @@ end
 """convert a stream of edges produced by sample into a graph"""
 function graph(edgestream, sizes, numedges::Int)
     g = Graph(sum(sizes))
-    println(g)
+    # println(g)
     count = 1
     for (i,j) in edgestream
-        #print("$count, $i,$j\n")
-        count += 1
+        # print("$count, $i,$j\n")
         if !has_edge(g,i,j)
-            unsafe_add_edge!(g,Edge(i,j))
+            add_edge!(g,Edge(i,j))
+            count += 1
         end
         if count >= numedges
             break
         end
     end
-    println(g)
+    # println(g)
     return g
 end
 
 """counts the number of edges that go between each block"""
 function blockcounts(sbm::StochasticBlockModel, A::AbstractMatrix)
-    info("making Q")
+    # info("making Q")
     I = collect(1:sbm.n)
     J =  [sbm.nodemap[i] for i in 1:sbm.n]
     V =  ones(sbm.n)
     Q = sparse(I,J,V)
-    #Q = Q / Q'Q
-    #@show Q'Q# < 1e-6
+    # Q = Q / Q'Q
+    # @show Q'Q# < 1e-6
     return (Q'A)*(Q)
 end
 
--- test/sbm.jl
+++ test/sbm.jl
@@ -1,65 +1,58 @@
+using Base.Test
 using LightGraphs
-import LightGraphs: nearbipartiteSBM, sample, StochasticBlockModel, graph, blockfractions
-#using ArgParse
-using PyPlot
+import LightGraphs: nearbipartiteSBM, sample, StochasticBlockModel, graph, blockfractions, blockcounts
 
-function main(filename, numedges, sizes)
+function generate_nbp_sbm(numedges, sizes)
     density = 1
-    print(STDERR, "Generating communites with sizes: $sizes\n")
-    between = density * 0.99
+    # print(STDERR, "Generating communites with sizes: $sizes\n")
+    between = density * 0.90
     intra = density * -0.005
     noise = density * 0.00501
     sbm = nearbipartiteSBM(sizes,between, intra, noise)
     edgestream = @task sample(sbm)
     g = graph(edgestream, sizes, numedges)
-    println(g)
-    open(filename, "w") do filep
-        writegraphml(filep, g)
-    end
     return sbm, g
 end
 
 
-println(STDERR, ARGS)
-argc = length(ARGS)
-progname = ARGS[1]
-if argc < 1
-    println(STDERR, "not enough arguments")
-end
+numedges = 100
+sizes = [10, 10, 10, 10]
 
-usagestr = "Usage: $progname outputfilename numedges size1 size2 ... sizeN\n"
-if argc < 5
-    println(STDERR, usagestr)
-    exit(1)
-end
-
-filename = ARGS[2]
-NUMEDGES = parse(Int, ARGS[3])
-sizes = [ parse(Int, i) for i in ARGS[4:end] ]
 n = sum(sizes)
-sbm, g = main(filename, NUMEDGES, sizes)
+sbm, g = generate_nbp_sbm(numedges, sizes)
+bc = blockcounts(sbm, g)
 bp = blockfractions(sbm, g) ./ (sizes * sizes')
 ratios = bp ./ (sbm.affinities ./ sum(sbm.affinities))
-@show norm(ratios)
-#println("Block counts:\n $bc")
-println("Block ratios:\n $ratios")
-spy(adjacency_matrix(g), markersize=1)
-title("Near Bipartite")
-show()
+@test norm(ratios) < 0.25
+# println("Block counts:\n $bc")
 
+sizes = [200, 200, 100]
 internaldeg = 15
-internalp = Float64[internaldeg/i for i in sizes]
 externaldeg = 6
+internalp = Float64[internaldeg/i for i in sizes]
 externalp = externaldeg/sum(sizes)
+numedges = internaldeg + externaldeg #+ sum(externaldeg.*sizes[2:end])
+numedges *= floor(Int, sum(sizes)/2)
 sbm = StochasticBlockModel(internalp, externalp, sizes)
 edgestream = @task sample(sbm)
-println(edgestream)
-g = graph(edgestream, sizes, NUMEDGES)
+g = graph(edgestream, sizes, numedges)
+@test ne(g) <= numedges
+@test nv(g) == sum(sizes)
+bc = blockcounts(sbm, g)
 bp = blockfractions(sbm, g) ./ (sizes * sizes')
 ratios = bp ./ (sbm.affinities ./ sum(sbm.affinities))
-@show norm(ratios)
-println("Block counts:\n $ratios")
-@show degree(g)
-spy(adjacency_matrix(g), markersize=1)
-title("SBM: \$$internaldeg\$,\$$externalp\$")
-show()
+@test norm(ratios) < 0.25
+# println("Block counts:\n $bc")
+# check that average degree is not too high
+# factor of two is cushion for random process
+@test mean(degree(g)) < 4//2*numedges/sum(sizes)
+# check that the internal degrees are higher than the external degrees
+# 5//4 is cushion for random process.
+@test all(sum(bc-diagm(diag(bc)), 1) .<= 5//4 .* diag(bc))
+
+
+sbm2 = StochasticBlockModel(0.5*ones(4), 0.3, 10*ones(Int,4))
+sbm  = StochasticBlockModel(0.5, 0.3, 10, 4)
+@test sbm == sbm2
+sbm.affinities[1,1] = 0
+@test sbm != sbm2
